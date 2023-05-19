add201426a51db834ffb84e36e227a6816433428
unaigomez <ungo895@vidalibarraquer.net>
2023-05-19 12:12:16 +0200

    afegir v1

--- mates1.java
+++ mates1.java
@@ -1,1 +1,12 @@
-aa
+import java.util.List;
+
+
+public class mates1 {
+    public static double calcularMedia(List<Double> numeros) {
+        double suma = 0.0;
+        for (double numero : numeros) {
+            suma += numero;
+        }
+        return suma / numeros.size();
+    }
+}
